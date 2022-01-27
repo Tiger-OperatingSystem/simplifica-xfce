b7bc7e8d5e5d162c5e49e838f275cd5d8c34773a
Natanael <natanaelgplay@gmail.com>
2022-01-26 21:06:38 -0300

    Update launchers_to_hide.sh

--- lists/launchers_to_hide.sh
+++ lists/launchers_to_hide.sh
@@ -1,2 +1,10 @@
 # Lançadores que devem ser ocultos do menu de aplicativos
+launchers_to_hide+=("thunar.desktop")
+launchers_to_hide+=("xfce4-session-logout.desktop")
+launchers_to_hide+=("vim.desktop")
+launchers_to_hide+=("debian-uxterm.desktop")
+launchers_to_hide+=("debian-xterm.desktop")
+launchers_to_hide+=("yad-icon-browser.desktop")
+launchers_to_hide+=("xfce4-about.desktop")
+launchers_to_hide+=("display-im6.q16.desktop")
 
