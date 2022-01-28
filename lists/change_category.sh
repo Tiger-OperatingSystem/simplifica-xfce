ffe067bad53f101c3f661d271cd03e1860725ef5
Natanael <natanaelgplay@gmail.com>
2022-01-28 16:53:52 -0300

    Update change_category.sh

--- lists/change_category.sh
+++ lists/change_category.sh
@@ -1,2 +1,9 @@
 # Trechos de script que trocam a categoria do aplicativo
 
+#----- Esse trecho troca a categoria do arquivo synaptic.desktop para Configurações ----#
+
+ sed -i 's|^Categories=.*|Categories=Settings;X-XFCE-SettingsDialog;X-XFCE-SystemSettings;|g' synaptic.desktop
+ 
+#----- Fim do trecho relacionado ao arquivo synaptic.desktop ----#
+ 
+ 
