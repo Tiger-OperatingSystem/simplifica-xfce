28a734e10e4817c7541b851c37143622e46ed054
Natanael <natanaelgplay@gmail.com>
2022-01-28 17:33:12 -0300

    Update modify_execution_environment.sh

--- lists/modify_execution_environment.sh
+++ lists/modify_execution_environment.sh
@@ -1,2 +1,17 @@
 # Trechos de script que modificam a execução do aplicativo
 
+#----- Esse trecho adiciona '--password-store=basic' como primeiro parametro no arquivo google-chrome.desktop ----#
+
+line=$(cat google-chrome.desktop | grep -n -A 10000 -E '^\[Desktop Entry]|^Exec=' | grep -m1 Exec= | cut -d\: -f1)
+
+command_line=$(sed -n ${line}p google-chrome.desktop  | cut -c 6- | sed 's/^[[:blank:]]*//;s/[[:blank:]]*$//')
+
+parameters=$(echo ${command_line} | sed 's/[^ ]* //')
+command=$(echo ${command_line} | sed 's|[[:space:]].*||g')
+
+parameters="--password-store=basic ${parameters}"
+  
+sed -i "${line}s|^Exec=.*|Exec=${command} ${parameters}|g" google-chrome.desktop
+
+#----- Fim do trecho relacionado ao arquivo google-chrome.desktop ----#
+
