12c980dc0d9e125928171ba4a1eb254705bf4f38
Daigo <openwebosbr@gmail.com>
2022-06-05 11:40:34 -0300

    Update launchers_to_manually_rename.sh
    Alterando nome 3 componentes FreeOffice

--- lists/launchers_to_manually_rename.sh
+++ lists/launchers_to_manually_rename.sh
@@ -1,2 +1,5 @@
 # Lançadores de aplicativos que devem ter o nome definidos manualmente
 
+sed "/^Name\[/d;s|^Name=.*|Name=Planilha (Excel)|g" planmaker-free21.desktop
+sed "/^Name\[/d;s|^Name=.*|Name=Slides (Power Point)|g" presentations-free21.desktop
+sed "/^Name\[/d;s|^Name=.*|Name=Texto (Word)|g" textmaker-free21.desktop
