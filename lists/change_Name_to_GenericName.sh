ef162f075c29a7e6248fe23f2778fcf66250a68e
Daigo <openwebosbr@gmail.com>
2022-06-07 09:14:03 -0300

    Update change_Name_to_GenericName.sh
    Corrigindo instruções colocadas erradas

--- lists/change_Name_to_GenericName.sh
+++ lists/change_Name_to_GenericName.sh
@@ -3,7 +3,3 @@ launchers_to_swap+=("mousepad.desktop")
 launchers_to_swap+=("atril.desktop")
 launchers_to_swap+=("thunar.desktop")
 launchers_to_swap+=("gparted.desktop")
-sed "/^Name\[/d;s|^Name=.*|Name=Gerenciador de servidores|g" gigolo.desktop
-sed "/^Name\[/d;s|^Name=.*|Name=Planilha (Excel)|g" planmaker-free21.desktop
-sed "/^Name\[/d;s|^Name=.*|Name=Slides (Power Point)|g" presentations-free21.desktop
-sed "/^Name\[/d;s|^Name=.*|Name=Texto (Word)|g" textmaker-free21.desktop
