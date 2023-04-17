751dd86b02a54f65bc698957eaa61a905e086447
Daigo <openwebosbr@gmail.com>
2023-04-17 09:52:31 -0300

    Update launchers_to_show.sh
    Tentando corrigir os .desktops ocultos errôneamente

--- lists/launchers_to_show.sh
+++ lists/launchers_to_show.sh
@@ -1,4 +1,4 @@
 # Lançadores ocultos que devem ser mostrados no menu de aplicativos
 
-launchers_to_show+=("boot-repair.desktop")
 launchers_to_show+=("synaptic.desktop")
+launchers_to_show+=("boot-repair.desktop")
