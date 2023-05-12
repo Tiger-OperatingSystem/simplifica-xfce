887f608501b563143f7a417bc889ba646308dff0
Natanael <natanaelgplay@gmail.com>
2023-05-12 10:19:49 -0300

    Update change_Name_to_GenericName.sh

--- lists/change_Name_to_GenericName.sh
+++ lists/change_Name_to_GenericName.sh
@@ -1,5 +1,3 @@
 # Lançadores cujo campo "Name" deve ser trocado pelo campo "GenericName"
-launchers_to_swap+=("mousepad.desktop")
-launchers_to_swap+=("atril.desktop")
 launchers_to_swap+=("thunar.desktop")
 launchers_to_swap+=("gparted.desktop")
