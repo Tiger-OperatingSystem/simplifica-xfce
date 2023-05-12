89ee0e9d6aa7ab88905ed92113a3a9891bb51fb8
Natanael <natanaelgplay@gmail.com>
2023-05-12 09:04:48 -0300

    Update change_category.sh

--- lists/change_category.sh
+++ lists/change_category.sh
@@ -3,7 +3,18 @@
 #----- Esse trecho troca a categoria do arquivo synaptic.desktop para Configurações ----#
 
  sed -i 's|^Categories=.*|Categories=Settings;X-XFCE-SettingsDialog;X-XFCE-SystemSettings;|g' synaptic.desktop
- 
+
 #----- Fim do trecho relacionado ao arquivo synaptic.desktop ----#
  
+#----- Esse trecho troca a categoria do arquivo atril.desktop para Acessórios ----#
+
+ sed -i 's|^Categories=.*|Categories=Utility;|g' atril.desktop
+ 
+ #----- Fim do trecho relacionado ao arquivo atril.desktop ----#
+ 
+ #----- Esse trecho troca a categoria do arquivo org.gnome.meld.desktop para Acessórios ----#
+
+ sed -i 's|^Categories=.*|Categories=Utility;|g' org.gnome.meld.desktop
+ 
+ #----- Fim do trecho relacionado ao arquivo org.gnome.meld.desktop ----#
  
